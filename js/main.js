2b96c63d7cd36498c9157e43734b07dd095123bb
Jay Powell <jay.powellcs@gmail.com>
2022-05-03 15:14:31 -0500

    education/work

--- js/main.js
+++ js/main.js
@@ -49,3 +49,24 @@ const navMenu = document.getElementById('nav-menu'),
  skillsHeader.forEach((el) =>{
      el.addEventListener('click', toggleSkills)
  })
+
+ /* Qualification Tabs */ 
+
+ const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelectorAll(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach( tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
