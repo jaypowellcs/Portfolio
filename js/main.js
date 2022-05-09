cd648656d4f1d9f622f4dfe237d70003fa814af3
Jay Powell <jay.powellcs@gmail.com>
2022-05-08 22:08:23 -0500

    scroll

--- js/main.js
+++ js/main.js
@@ -109,3 +109,12 @@ let swiper = new Swiper(".portfolio__container", {
   }
 
   window.addEventListener('scroll', scrollActive)
+
+  /* Change Background Header */ 
+
+  function scrollHeader() {
+      const nav = document.getElementById('header')
+      if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+  }
+
+  window.addEventListener('scroll', scrollHeader)
